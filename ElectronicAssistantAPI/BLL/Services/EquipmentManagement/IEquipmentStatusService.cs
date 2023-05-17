d6b0d2c1b8662e9a38e991c44c223de3982fea3d
Oleg Nekrasov <79853886592@yandex.ru>
2023-05-17 11:22:11 +0300

    Added EquipmentStatusService

---
+++ ElectronicAssistantAPI/BLL/Services/EquipmentManagement/IEquipmentStatusService.cs
@@ -0,0 +1,14 @@
+﻿using ElectronicAssistantAPI.BLL.Models.EquipmentManagement.EquipmentStatus;
+using ElectronicAssistantAPI.DAL.Models.EquipmentManagement;
+
+namespace ElectronicAssistantAPI.BLL.Services.EquipmentManagement
+{
+    public interface IEquipmentStatusService
+    {
+        IEnumerable<EquipmentStatus> Get();
+        Task<EquipmentStatus> GetByIdAsync(string id);
+        Task<EquipmentStatus> AddAsync(AddEquipmentStatus model);
+        Task<EquipmentStatus> UpdateAsync(UpdateEquipmentStatus model);
+        Task DeleteAsync(DelEquipmentStatus model);
+    }
+}
